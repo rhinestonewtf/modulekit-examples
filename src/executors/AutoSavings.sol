7788dcb3a1b171930580064d2fe11aa15ce98dcd
zeroknots <102132718+zeroknots@users.noreply.github.com>
2023-11-07 10:27:57 +0700

    Feature/autosavings (#5)
    * feat: autosaver module
    
    
    ---------
    
    Co-authored-by: kopy-kat <kopp.konrad@gmail.com>

---
+++ src/executors/AutoSavings.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import "modulekit/modulekit/ConditionalExecutorBase.sol";
+import "modulekit/modulekit/integrations/uniswap/v3/UniswapSwaps.sol";
+import "modulekit/modulekit/integrations/erc4626/ERC4626Deposit.sol";
+import "modulekit/modulekit/interfaces/IExecutor.sol";
+import "checknsignatures/CheckNSignatures.sol";
+
+import "../validators/SessionKey/ISessionKeyValidationModule.sol";
+
+import "solady/utils/LibSort.sol";
+
+struct TokenTxEvent {
+    address token;
+    address to;
+}
+
+contract AutoSavings is ConditionalExecutor, ISessionKeyValidationModule {
+    using ModuleExecLib for IExecutorManager;
+    using ERC4626Deposit for IERC4626;
+    using LibSort for address[];
+
+    struct SavingsConfig {
+        IERC4626 vault;
+        uint256 maxAmountIn;
+        uint48 lastTriggered;
+        uint16 feePercentage;
+        uint256 maxFee;
+    }
+
+    struct AuthorizedRelay {
+        address[] authorizedSigners;
+        uint256 threshold;
+    }
+
+    mapping(address account => mapping(bytes32 id => SavingsConfig)) public savingsConfig;
+
+    mapping(address account => AuthorizedRelay) authorizedRelay;
+
+    error InvalidConfig(address account, bytes32 id);
+    error SavingNotDue(address account, bytes32 id);
+    error InvalidTarget();
+    error InvalidAmount();
+    error InvalidToken();
+    error InvalidTxEventTo();
+    error InvalidFunctionSelector();
+
+    event AutoSavingsTx(
+        bytes32 id,
+        address vault,
+        address spendToken,
+        address saveToken,
+        uint256 amountReceived,
+        uint256 amountSaved
+    );
+    event NewRelayer(address account, address[] relayer, uint256 threshold);
+
+    constructor(ComposableConditionManager _conditionManager)
+        ConditionalExecutor(_conditionManager)
+    { }
+
+    function trigger(
+        IERC20 spendToken,
+        IExecutorManager manager,
+        bytes32 id,
+        uint256 amountTransfered,
+        ConditionConfig[] calldata conditions
+    )
+        external
+        onlyIfConditionsMet(msg.sender, conditions)
+    {
+        SavingsConfig storage config = savingsConfig[msg.sender][id];
+
+        uint256 amountIn = config.feePercentage * amountTransfered / 10_000;
+        if (amountIn > config.maxAmountIn) revert InvalidConfig(msg.sender, id);
+
+        // check of swap is required
+        IERC20 vaultToken = IERC20(config.vault.asset());
+        if (vaultToken != spendToken) {
+            uint256 amountOut = vaultToken.balanceOf(msg.sender);
+
+            ExecutorAction[] memory swapActions = new ExecutorAction[](2);
+            swapActions[0] = ERC20ModuleKit.approveAction({
+                token: spendToken,
+                to: SWAPROUTER_ADDRESS,
+                amount: amountIn
+            });
+            swapActions[1] = UniswapSwaps.swapExactInputSingle({
+                smartAccount: msg.sender, // beneficiary of the swap
+                tokenIn: spendToken, // token to be sold
+                tokenOut: vaultToken, // token to be bought
+                amountIn: amountIn
+            });
+
+            // this execution could  be provided via calldata / storage
+            manager.exec({ account: msg.sender, actions: swapActions });
+
+            amountIn = vaultToken.balanceOf(msg.sender) - amountOut;
+        }
+
+        // // deposit into vault
+        config.vault.approveAndDeposit({
+            manager: manager,
+            account: msg.sender,
+            receiver: msg.sender,
+            amount: amountIn
+        });
+
+        emit AutoSavingsTx({
+            id: id,
+            vault: address(config.vault),
+            spendToken: address(spendToken),
+            saveToken: address(vaultToken),
+            amountReceived: amountTransfered,
+            amountSaved: amountIn
+        });
+    }
+
+    function setRelayer(address[] memory relayer, uint256 threshold) external {
+        relayer.uniquifySorted();
+
+        if (relayer.length < threshold) revert InvalidConfig(msg.sender, 0);
+
+        AuthorizedRelay storage authorizedRelayRecord = authorizedRelay[msg.sender];
+        authorizedRelayRecord.threshold = threshold;
+        authorizedRelayRecord.authorizedSigners = relayer;
+
+        emit NewRelayer(msg.sender, relayer, threshold);
+    }
+
+    function setConfig(
+        bytes32 id,
+        uint256 maxAmountIn,
+        uint16 feePercentage,
+        IERC4626 vault
+    )
+        external
+    {
+        SavingsConfig storage config = savingsConfig[msg.sender][id];
+
+        config.maxAmountIn = maxAmountIn;
+        config.vault = vault;
+        config.feePercentage = feePercentage;
+    }
+
+    function validateSessionUserOp(
+        UserOperation calldata _op,
+        bytes32 _userOpHash,
+        bytes calldata _sessionKeyData,
+        bytes calldata _sessionKeySignature
+    )
+        external
+        view
+        override
+        returns (bool)
+    {
+        {
+            if (address(this) != address(bytes20(_op.callData[48:68]))) revert InvalidTarget();
+        }
+
+        bytes calldata triggerCallData = (_op.callData[228:]);
+        TokenTxEvent memory tokenTxEvent;
+        {
+            bytes4 functionSig = bytes4(triggerCallData[0:4]);
+            address spendToken = address(bytes20(triggerCallData[16:36]));
+            //uint256 amountIn = uint256(bytes32(triggerCallData[100:132]));
+
+            tokenTxEvent = abi.decode(_sessionKeyData, (TokenTxEvent));
+            //if (tokenTxEvent.amount != amountIn) revert InvalidAmount();
+            if (tokenTxEvent.token != spendToken) revert InvalidToken();
+            if (functionSig != this.trigger.selector) revert InvalidFunctionSelector();
+
+            if (tokenTxEvent.to != _op.sender) revert InvalidTxEventTo();
+        }
+
+        AuthorizedRelay storage authorizedRelayRecord = authorizedRelay[_op.sender];
+        address[] memory recoveredSigners = CheckSignatures.recoverNSignatures({
+            dataHash: keccak256(_sessionKeyData),
+            signatures: _sessionKeySignature,
+            requiredSignatures: authorizedRelayRecord.threshold
+        });
+
+        recoveredSigners.uniquifySorted();
+
+        address[] memory authorizedRecoveredSigners =
+            recoveredSigners.intersection(authorizedRelayRecord.authorizedSigners);
+
+        if (authorizedRecoveredSigners.length >= authorizedRelayRecord.threshold) {
+            return true;
+        }
+
+        return false;
+    }
+
+    function name() external view override returns (string memory name) { }
+
+    function version() external view override returns (string memory version) { }
+
+    function metadataProvider()
+        external
+        view
+        override
+        returns (uint256 providerType, bytes memory location)
+    { }
+
+    function requiresRootAccess() external view override returns (bool requiresRootAccess) { }
+}
---
+++ test/executors/AutoSavings.t.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import { Test } from "forge-std/Test.sol";
+import {
+    RhinestoneModuleKit,
+    RhinestoneModuleKitLib,
+    RhinestoneAccount
+} from "modulekit/test/utils/safe-base/RhinestoneModuleKit.sol";
+import "modulekit/modulekit/interfaces/IExecutor.sol";
+
+import "modulekit/core/ComposableCondition.sol";
+import { AutoSavings, TokenTxEvent } from "../../src/executors/AutoSavings.sol";
+import "modulekit/test/mocks/MockRegistry.sol";
+import "solmate/test/utils/mocks/MockERC4626.sol";
+import "modulekit/test/mocks/MockCondition.sol";
+import "solmate/test/utils/mocks/MockERC20.sol";
+import "../MainnetFork.t.sol";
+import "murky/src/Merkle.sol";
+import { IERC4626 } from "modulekit/modulekit/integrations/interfaces/IERC4626.sol";
+import "forge-std/interfaces/IERC20.sol";
+import "../../src/validators/SessionKey/SessionKeyManager.sol";
+
+import "checknsignatures/CheckNSignaturesFoundryHelper.sol";
+
+address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+contract AutoSavingsTest is MainnetTest, RhinestoneModuleKit, CheckNSignaturesFoundryHelper {
+    using RhinestoneModuleKitLib for RhinestoneAccount;
+
+    RhinestoneAccount instance;
+    MockRegistry registry;
+    ComposableConditionManager conditionManager;
+    AutoSavings autoSavings;
+
+    MockCondition mockCondition;
+    IERC20 usdc = IERC20(USDC);
+    IERC20 weth = IERC20(WETH);
+    MockERC4626 vault;
+    Merkle m;
+
+    string constant keySalt = "0";
+    string constant keyName = "key";
+
+    address payer;
+
+    address[] relayerAddresses;
+    uint256[] relayerPks;
+
+    function _genSigners(uint256 n) internal {
+        relayerAddresses = new address[](n);
+        relayerPks = new uint256[](n);
+
+        uint256 offset = 1337;
+        for (uint256 i; i < n; i++) {
+            relayerAddresses[i] = vm.addr(i + offset);
+            relayerPks[i] = i + offset;
+        }
+    }
+
+    function setUp() public override {
+        super.setUp();
+
+        m = new Merkle();
+
+        _genSigners(3);
+        payer = makeAddr("payer");
+        // Setup account
+        instance = makeRhinestoneAccount("1");
+
+        vm.deal(instance.account, 10 ether);
+
+        registry = new MockRegistry();
+        conditionManager = new ComposableConditionManager(registry);
+        vault = new MockERC4626(ERC20(address(weth)), "vWETH", "vWETH");
+        mockCondition = new MockCondition();
+        vm.label(address(mockCondition), "GasCondition");
+        vm.label(instance.account, "account");
+        vm.label(WETH, "weth");
+        vm.label(USDC, "usdc");
+
+        // Setup executor
+        autoSavings = new AutoSavings(conditionManager);
+
+        // Add executor to account
+        instance.addExecutor(address(autoSavings));
+    }
+
+    function mockConditionConfig() internal view returns (ConditionConfig[] memory conditions) {
+        conditions = new ConditionConfig[](1);
+        conditions[0] =
+            ConditionConfig({ condition: ICondition(address(mockCondition)), conditionData: "" });
+    }
+
+    function testTrigger(uint256 amount) public {
+        vm.assume(amount > 10_000);
+        vm.assume(amount < 100_000 ether);
+        // metadata for session key
+        uint256 validUntil = 0;
+        uint256 validAfter = 180_000_000;
+        address tokenToSave = address(usdc);
+        bytes memory savingsEvent =
+            abi.encode(TokenTxEvent({ token: tokenToSave, to: instance.account }));
+
+        vm.startPrank(instance.account);
+        ConditionConfig[] memory conditions = mockConditionConfig();
+        conditionManager.setHash(address(autoSavings), conditions);
+        autoSavings.setConfig({
+            id: 0,
+            maxAmountIn: amount,
+            feePercentage: 100,
+            vault: IERC4626(address(vault))
+        });
+
+        // user authorizes a specific relayer to trigger autosavings
+        autoSavings.setRelayer(relayerAddresses, relayerAddresses.length);
+        (, bytes32[] memory proof) = instance.addSessionKey({
+            validUntil: validUntil,
+            validAfter: validAfter,
+            sessionValidationModule: address(autoSavings),
+            sessionKeyData: savingsEvent
+        });
+
+        vm.stopPrank();
+
+        // simulate a ERC20 transfer
+        deal(tokenToSave, payer, amount);
+        vm.prank(payer);
+        usdc.transfer(instance.account, amount);
+
+        // prepare sessionKeyParams for SessionKeyManager
+        SessionKeyParams memory sessionKeyParams = SessionKeyParams({
+            validUntil: validUntil,
+            validAfter: validAfter,
+            sessionValidationModule: address(autoSavings),
+            sessionKeyData: savingsEvent,
+            merkleProof: proof,
+            sessionKeySignature: sign(relayerPks, savingsEvent) // sign with CheckNSignaturesFoundryHelper
+         });
+
+        // trigger 4337 exec
+        instance.exec4337({
+            target: address(autoSavings),
+            value: 0,
+            callData: abi.encodeCall(
+                autoSavings.trigger,
+                (usdc, IExecutorManager(address(instance.aux.executorManager)), 0, amount, conditions)
+                ),
+            signature: ValidatorSelectionLib.encodeValidator({
+                signature: abi.encode(sessionKeyParams),
+                chosenValidator: address(sessionKeyManager)
+            })
+        });
+    }
+}
